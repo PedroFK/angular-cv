8634e12c6c9ea6115d9407e63eb3d8c489035e75
linuxpedro <pdro.dev97@gmail.com>
2024-02-19 17:35:32 -0300

    feat: components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 
+import { HomeComponent } from './modules/curriculum/pages/home/home.component';
+
 export const routes: Routes = [
     {
         path: '',
-        component:
+        component: HomeComponent
     }
 ];
