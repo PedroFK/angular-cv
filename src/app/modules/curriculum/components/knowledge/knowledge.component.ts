1ba3574e845a5b079a6a62b7e7d72e70896be49f
linuxpedro <pdro.dev97@gmail.com>
2024-02-21 10:00:14 -0300

    feat: knowledge component

--- src/app/modules/curriculum/components/knowledge/knowledge.component.ts
+++ src/app/modules/curriculum/components/knowledge/knowledge.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { SIGNAL } from '@angular/core/primitives/signals';
+import { knowLedge } from '../../interface/knowledge.interface';
 
 @Component({
   selector: 'app-knowledge',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './knowledge.component.html',
   styleUrl: './knowledge.component.scss'
 })
 export class KnowledgeComponent {
-
+  public arrayKnowledge = signal<knowLedge[]>([
+    {
+      src: 'assets/icons/knowledge/html5.svg',
+      alt: 'ícone html'
+    },
+    {
+      src: 'assets/icons/knowledge/css3.svg',
+      alt: 'ícone css'
+    },
+    {
+      src: 'assets/icons/knowledge/javascript.svg',
+      alt: 'ícone javascript'
+    },
+    {
+      src: 'assets/icons/knowledge/angular.svg',
+      alt: 'ícone angular'
+    },
+    {
+      src: 'assets/icons/knowledge/sass.svg',
+      alt: 'ícone sass'
+    },
+  ])
 }
